2144054125eaf7af2d907745bb94686c326f2230
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2025-02-08 14:30:21 +0700

    initialize project with TypeScript, Express, ESLint, and Prettier configuration

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'Application is running'
+    })
+});
+
+app.listen(port, () => {
+console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
